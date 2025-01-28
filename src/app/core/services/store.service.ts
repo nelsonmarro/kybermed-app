4cd855b03fec9660365034aff8830a59975ca8f6
Nelson Marro <nelsonmarro99@gmail.com>
2025-01-28 11:10:11 -0500

    /workspace/github/nelsonmarro/kybermed-app

--- src/app/core/services/store.service.ts
+++ src/app/core/services/store.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+import { Storage } from '@ionic/storage-angular';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -11,6 +13,14 @@ export class StoreService {
   }
 
   async init() {
+    // If using, define drivers here: await this.storage.defineDriver(/*...*/);
     const storage = await this.storage.create();
+    this._storage = storage;
+  }
+
+  // Create and expose methods that users of this service can
+  // call, for example:
+  public set(key: string, value: any) {
+    this._storage?.set(key, value);
   }
 }
