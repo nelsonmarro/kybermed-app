c93f60b220498c73024f44c3305808e4485c7979
Nelson Marro <nelsonmarro99@gmail.com>
2025-01-27 22:30:07 -0500

    /workspace/github/nelsonmarro/kybermed-app

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private authUrl = 'http://localhost:3000/api/auth';
+  private tokenKey = 'ACCESS_TOKEN';
 
+  private currentUser = signal();
   constructor() {}
 }
