da8f78ef81816e3222c1e8296476898f4646aea9
Nelson Marro <nelsonmarro99@gmail.com>
2025-01-28 11:20:07 -0500

    /workspace/github/nelsonmarro/kybermed-app

--- src/app/core/services/store.service.ts
+++ src/app/core/services/store.service.ts
@@ -13,13 +13,10 @@ export class StoreService {
   }
 
   async init() {
-    // If using, define drivers here: await this.storage.defineDriver(/*...*/);
     const storage = await this.storage.create();
     this._storage = storage;
   }
 
-  // Create and expose methods that users of this service can
-  // call, for example:
   public set(key: string, value: any) {
     this._storage?.set(key, value);
   }
