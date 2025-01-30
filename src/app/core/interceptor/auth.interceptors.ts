3fcae1a0711fd20857e01617009d6458c4b2c708
Nelson Marro <nelsonmarro99@gmail.com>
2025-01-30 17:00:07 -0500

    /workspace/github/nelsonmarro/kybermed-app

--- src/app/core/interceptor/auth.interceptors.ts
+++ src/app/core/interceptor/auth.interceptors.ts
@@ -6,5 +6,13 @@ export function authInterceptor(
   req: HttpRequest<unknown>,
   next: HttpHandlerFn,
 ) {
-  const authToke = inject(StorageService).get('ACCESS_TOKEN');
+  const token = inject(StorageService).get('ACCESS_TOKEN');
+
+  if (token) {
+    const newReq = req.clone({
+      headers: req.headers.append('Authorization', `Bearer ${token}`),
+    });
+    return next(newReq);
+  }
+  return next(req);
 }
